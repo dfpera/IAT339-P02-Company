b3d8e945cac1e52395547b959967b64f9f0fe31c
Daniele Perazzolo <dperazzo@sfu.ca>
2017-02-21 15:52:27 -0800

    Set up basic Map page working with google map api.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
 // Main js file contains all interactions that will be present within the page.
+
+// Functions
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: {lat: 49.2589637, lng: -123.0648282}
+  });
+  // Create an array of alphabetical characters used to label the markers.
+  var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  // Add some markers to the map.
+  // Note: The code uses the JavaScript Array.prototype.map() method to
+  // create an array of markers based on a given "locations" array.
+  // The map() method here has nothing to do with the Google Maps API.
+  var markers = locations.map(function(location, i) {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length],
+      map: map
+    });
+  });
+}
+var locations = [
+  {lat: 49.249510, lng: -123.104452},
+  {lat: 49.278678, lng: -123.113368},
+  {lat: 49.272966, lng: -123.002818}
+];
+
+// Main Loop
 $(function() {
   // Old js
   document.getElementById('modal-open').onclick=function(){
